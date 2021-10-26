2039d3a7ee9554e69af4d5ae46ec97b1a1d97c3e
GabrielDpb <92184744+GabrielDpb@users.noreply.github.com>
2021-10-26 14:16:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function listaordenada()	{
+			escrever = document.querySelector('ol')
+			escrever.setAttribute('id', 'inicio')
+			
+			const encontrar = document.querySelectorAll('h2');
+			const elementos = new Array();
+			encontrar.forEach(resultado => { elementos.push(resultado.textContent)
+			const inicio = document.createElement('a')
+			inicio.setAttribute('name', resultado.textContent);
+			inicio.setAttribute('href', '#inicio');
+			inicio.textContent = "início"
+			resultado.append(inicio);
+			} );
+
+			
+			elementos.forEach(topico => {
+				lista = document.createElement('li');
+				escrever.append(lista);
+				const ancora = document.createElement('a')
+				ancora.setAttribute('href', `#${topico}`)
+				ancora.textContent = topico
+				lista.append(ancora)
+			})
+		
+}
